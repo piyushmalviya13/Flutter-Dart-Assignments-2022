170621e0a29a18391046ae9594327f844d6fd1b0
Piyush Malviya <piyush.malviya@gonulcei.com>
2022-01-06 16:39:16 +0530

    implement assignment1

--- assignment1/bin/assignment1.dart
+++ assignment1/bin/assignment1.dart
@@ -6,16 +6,28 @@ void main() {
   String choice = 'y';
   while (choice == 'y') {
     print(
-        'Please enter the items in the format below separating each property with a comma');
+        '---------------------------------------------------------------------------------------------------------------------------------------------');
     print(
-        'name <item name>,price <item price>,quantity <item quantity>,type <item type>');
+        '\nPlease enter the items in the format below separating each property with a comma\n');
     print(
-        'The item type can be any of the following 1.raw 2.manufactured 3.imported');
+        '-name <item name>,-price <item price>,-quantity <item quantity>,-type <item type>\n');
+    print(
+        'The item type can be any of the following 1.raw 2.manufactured 3.imported\n');
     String inputItem = stdin.readLineSync()!;
     if (checkValidInput(inputItem)) {
-      // Item item = getInputItem();
+      Item item = returnItemObject(inputItem);
+      item.calculateTax();
+      print(
+          "\nThe ${item.getType()} item ${item.getName()} is priced at ${item.getPrice()} and for quantity ${item.getQuantity()} will be taxed amount ${item.getTaxAmount()} per  => Total Amount: ${item.getTotalAmount()}\n");
+      print(
+          '---------------------------------------------------------------------------------------------------------------------------------------------');
+      print("Do you want to enter details of any other item (y/n): ");
+      choice = stdin.readLineSync()!;
     } else {
-      print('Please enter the details in a valid format');
+      print('\nPlease enter the details in a valid format\n');
+      print(
+          '---------------------------------------------------------------------------------------------------------------------------------------------');
     }
   }
+  print("\nThanks For Visiting\n");
 }
--- assignment1/lib/checkValidInput.dart
+++ assignment1/lib/checkValidInput.dart
@@ -1,8 +1,64 @@
+import './item.dart';
+
 List splitStringByComma(String input) {
   return input.split(',');
 }
 
 bool checkValidInput(String input) {
-  List splitinput = splitStringByComma(input);
-  return true;
+  List splitInput = splitStringByComma(input);
+
+  if (splitInput.length != 4 || splitInput[0].split(' ')[0] != '-name') {
+    return false;
+  }
+
+  bool priceFound = false, quantityFound = false, typeFound = false;
+  String price = '', quanity = '', type = '';
+  for (String detail in splitInput) {
+    if (detail.split(' ')[0] == '-price') {
+      priceFound = true;
+      price = detail.split(' ')[1];
+    } else if (detail.split(' ')[0] == '-quantity') {
+      quantityFound = true;
+      quanity = detail.split(' ')[1];
+    } else if (detail.split(' ')[0] == '-type') {
+      typeFound = true;
+      type = detail.split(' ')[1];
+      if (type != 'raw' && type != 'manufactured' && type != 'imported') {
+        return false;
+      }
+    }
+  }
+
+  if (priceFound && quantityFound && typeFound) {
+    try {
+      double.parse(price);
+      int.parse(quanity);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  } else {
+    return true;
+  }
+}
+
+Item returnItemObject(String input) {
+  List splitInput = splitStringByComma(input);
+
+  String price = '', quanity = '', type = '';
+  for (String detail in splitInput) {
+    if (detail.split(' ')[0] == '-price') {
+      price = detail.split(' ')[1];
+    } else if (detail.split(' ')[0] == '-quantity') {
+      quanity = detail.split(' ')[1];
+    } else if (detail.split(' ')[0] == '-type') {
+      type = detail.split(' ')[1];
+    }
+  }
+
+  String itemName = splitInput[0].split(' ')[1];
+  double itemPrice = double.parse(price);
+  int itemQuantity = int.parse(quanity);
+
+  return Item(itemName, itemPrice, itemQuantity, type);
 }
